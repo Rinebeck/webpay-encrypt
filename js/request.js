a2e9ca79ee7899c067d6e699e2b5696900ed1973
apedroza <ponchopdev@gmai.com>
2022-05-13 19:55:14 -0500

    Return result instead of console log

--- js/request.js
+++ js/request.js
@@ -1,20 +1,20 @@
 const ENDPOINT_URL = "https://clickclack.mx/encrypt/";
 
 const getPaymentURL = (amount, email) => {
-var formData = new FormData();
-formData.append("action", "get_url");
-formData.append("transaction_type", "single_exhibition");
-formData.append("amount", amount);
-formData.append("email", email);
+  var formData = new FormData();
+  formData.append("action", "get_url");
+  formData.append("transaction_type", "single_exhibition");
+  formData.append("amount", amount);
+  formData.append("email", email);
 
-var requestOptions = {
-  method: 'POST',
-  body: formData,
-  redirect: 'follow'
-};
+  var requestOptions = {
+    method: "POST",
+    body: formData,
+    redirect: "follow",
+  };
 
-return fetch(ENDPOINT_URL, requestOptions)
-  .then(response => response.json())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+  return fetch(ENDPOINT_URL, requestOptions)
+    .then((response) => response.json())
+    .then((result) => result)
+    .catch((error) => console.log("error", error));
 };
