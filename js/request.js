a175c419102f07a9df5926b22ad1c3979647d8b6
apedroza <ponchopdev@gmai.com>
2022-05-13 20:31:43 -0500

    more changes, who cares

--- js/request.js
+++ js/request.js
@@ -1,9 +1,9 @@
 const ENDPOINT_URL = "https://clickclack.mx/encrypt/";
 
-const getPaymentURL = (amount, email) => {
+const getPaymentURL = (amount, transaction_type, email) => {
   var formData = new FormData();
   formData.append("action", "get_url");
-  formData.append("transaction_type", "single_exhibition");
+  formData.append("transaction_type", transaction_type);
   formData.append("amount", amount);
   formData.append("email", email);
 
@@ -17,4 +17,46 @@ const getPaymentURL = (amount, email) => {
     .then((response) => response.json())
     .then((result) => result.response)
     .catch((error) => console.log("error", error));
+};
+
+document.querySelector("#webpayplus").addEventListener("click", () => {
+  const email = document.querySelector('[name="email"]').value;
+  const amount = parseFloat(
+    document
+      .querySelector(".w-commerce-commercecheckoutsummarytotal")
+      .innerText.replace(/[^0-9.]/g, "")
+  );
+  if (email && amount) {
+    const response = getPaymentURL(amount, "deferred_charge", email);
+    response.then((response) => {
+      const iframeUrl = response.nb_url;
+      // create a dialog with iframe inside
+      const dialog = document.createElement("dialog");
+      dialog.setAttribute("id", "paymentDialog");
+      const iframe = document.createElement("iframe");
+      iframe.src = iframeUrl;
+      iframe.style = "overflow:hidden;height:100%;width:100%";
+      iframe.setAttribute("frameborder", "0");
+      iframe.setAttribute("width", "100%");
+      iframe.setAttribute("height", "100%");
+
+      dialog.appendChild(iframe);
+      document.body.appendChild(dialog);
+      loadCancelButton();
+      dialog.showModal();
+    });
+  } else {
+    alert("Please enter your email.");
+  }
+});
+
+const loadCancelButton = () => {
+  const cancelButton = document.createElement("button");
+  const dialog = document.getElementById("paymentDialog");
+  cancelButton.setAttribute("id", "cancelButton");
+  cancelButton.innerText = "Cancelar";
+  cancelButton.addEventListener("click", function () {
+    dialog.close();
+  });
+  dialog.appendChild(cancelButton);
 };
